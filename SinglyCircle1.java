c36a8bae18ed9c57959990930001a3307a1f1c78
linsang90 <linsang90@gmail.com>
2014-09-14 20:14:42 -0400

    1st

---
+++ SinglyCircle1.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode rubbit = head;
+        ListNode tortoise = head;
+        while ((rubbit != null && rubbit.next != null) && tortoise != null) {
+            rubbit = rubbit.next.next;
+            tortoise = tortoise.next;
+            if(rubbit == tortoise) return true;
+        }
+        return false;
+        
+    }
+}
+

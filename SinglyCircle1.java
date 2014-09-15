bb755fd700a452e0a759d7c28c083f0de7d1f934
linsang90 <linsang90@gmail.com>
2014-09-15 16:33:23 -0400

    `

--- SinglyCircle1.java
+++ SinglyCircle1.java
@@ -13,7 +13,7 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode rubbit = head;
         ListNode tortoise = head;
-        while ((rubbit != null && rubbit.next != null) && tortoise != null) {
+        while ((rubbit != null && rubbit.next != null) && tortoise != null) { 
             rubbit = rubbit.next.next;
             tortoise = tortoise.next;
             if(rubbit == tortoise) return true;

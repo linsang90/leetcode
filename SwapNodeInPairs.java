3d9091f61b276c8850a88c2ddfdc8735b80979de
linsang90 <linsang90@gmail.com>
2014-09-16 23:26:09 -0400

    best answer so far

---
+++ SwapNodeInPairs.java
@@ -0,0 +1,26 @@
+tion for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+  public ListNode swapPairs(ListNode head) {
+    ListNode start = new ListNode(0); //make head no longer a special case
+    start.next = head;
+
+    for(ListNode cur = start; cur.next != null && cur.next.next != null; cur = cur.next.next) {
+      cur.next = swap(cur.next, cur.next.next);        
+    }
+    return start.next;
+  }
+  private ListNode swap(ListNode next1, ListNode next2) {
+    next1.next = next2.next;
+    next2.next = next1;
+    return next2;
+  }
+}

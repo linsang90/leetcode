279c8202a4c2b7dc8238f2231d1f18a547b770cb
linsang90 <linsang90@gmail.com>
2014-12-30 23:12:45 -0500

    O(n)time,O(1)space

---
+++ TwoSumII_2.java
@@ -0,0 +1,17 @@
+public class Solution {
+	    public int[] twoSumII(int[] numbers, int target) {
+	    	int i = 0,j = numbers.length-1;
+	    	while(i<j) {
+	    		if(numbers[i]+numbers[j]>target) {
+	    			j--;
+	    		}
+	    		else if(numbers[i]+numbers[j]<target){
+	    			i++;
+	    		}
+	    		else {
+	    			return new int[] {i+1,j+1};
+	    		}
+	    	}
+	    	return null;
+	    }
+	}

faf2344df58d178cab82184cfd5d615831ecb9c8
Sang Lin <linsang90@gmail.com>
2015-06-16 16:00:15 -0400

    straight

---
+++ RotateImage.java
@@ -0,0 +1,11 @@
+public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i=0;i<n/2;i++)
+            for(int j=i;j<n-1-i;j++) {
+                int tmp = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+  }

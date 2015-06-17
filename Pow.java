c89085eea1d8208b66a157dd91cea05347028ca9
Sang Lin <linsang90@gmail.com>
2015-06-17 11:35:09 -0400

    binary divide

---
+++ Pow.java
@@ -0,0 +1,9 @@
+public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        
+        if(n%2 == 0) return myPow(x*x,n/2);
+        else {
+            if(n>0) return x*myPow(x,n-1);
+            else return 1/x*myPow(x,n+1);
+        }
+    }

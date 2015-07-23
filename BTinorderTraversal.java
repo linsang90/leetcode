e71fcbb3180986c7c86946f6553b4e6f91503fab
Sang Lin <linsang90@gmail.com>
2015-07-23 14:31:01 -0400

    recursive

---
+++ BTinorderTraversal.java
@@ -0,0 +1,9 @@
+List<Integer> list = new ArrayList<Integer>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root != null) {
+            inorderTraversal(root.left);
+            list.add(root.val);
+            inorderTraversal(root.right);
+        }
+        return list;
+    }

e93fc1786887a219c2cb908b1110fe877fb12eea
Sang Lin <linsang90@gmail.com>
2015-07-23 14:55:37 -0400

    dp

---
+++ UniqueBST.java
@@ -0,0 +1,11 @@
+public int numTrees(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++)
+            for(int k=1;k<=i;k++) {
+                dp[i] += dp[k-1] * dp[i-k];
+            }
+        return dp[n];
+    }

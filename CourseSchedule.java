52b9712ef3bfb799bc84458721e6e7e9adb5aede
Sang Lin <linsang90@gmail.com>
2015-08-21 23:39:43 -0400

    bfs

---
+++ CourseSchedule.java
@@ -0,0 +1,30 @@
+public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(numCourses == 0 || prerequisites.length < 1) return true;
+        int[] indegree = new int[numCourses];
+        int cnt = numCourses;
+        Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+        for (int i = 0; i < numCourses; i++) {
+            map.put(i, new ArrayList<Integer>());
+        }
+        for(int i = 0; i < prerequisites.length; i++)  {
+               map.get(prerequisites[i][0]).add(prerequisites[i][1]);
+               indegree[prerequisites[i][1]]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        
+        while(!queue.isEmpty()) {
+            int current = queue.poll();
+            for(int i : map.get(current)) {
+                if(--indegree[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+            cnt--;
+        }
+        return cnt == 0;
+    }

2465265b4551e570cabc28f8295819f3467858cd
Sang Lin <linsang90@gmail.com>
2015-08-23 15:13:27 -0400

    2 lines

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -1,16 +1,4 @@
 public boolean isPalindrome(String s) {
-	    	int i = 0,j = s.length();
-	    	while(i<j) {
-	    		while(i<j&&!Character.isLetterOrDigit(i)) {
-	    			i++;
-	    		}
-	    		while(i<j&&!Character.isLetterOrDigit(j)) {
-	    			j--;
-	    		}
-	    		if(Character.toLowerCase(s.charAt(i))
-	    				!=Character.toLowerCase(s.charAt(j)))
-	    			return false;
-	    		i++;j--;
-	    	}
-	    	return true;
-	    }
+	s=s.toLowerCase().replaceAll("[^a-z0-9]", "");
+	return new StringBuilder(s).reverse().toString().equals(s);
+}

c6f67c74c131c09a4b7455407cf933d78ef30534
Sang Lin <linsang90@gmail.com>
2015-09-07 15:47:09 -0400

    merge sort

---
+++ SortList.java
@@ -0,0 +1,38 @@
+public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode prev = null, fast = head, slow = head;
+        while(fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        prev.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        
+        return merge(l1,l2);
+        
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        
+        if(l1 != null) curr.next = l1;
+        if(l2 != null) curr.next = l2;
+        
+        return dummyHead.next;
+    }

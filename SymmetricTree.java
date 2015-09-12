0c0cfb4e78b2d203a358ce119f127a2785f2dc62
Sang Lin <linsang90@gmail.com>
2015-09-11 20:19:35 -0400

    recusive

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -1,18 +1,11 @@
-public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> lists = new LinkedList<List<Integer>>();
-        if(root == null) return lists;
-        
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.offer(root);
-        while(!queue.isEmpty()) {
-            int levelNum = queue.size();
-            List<Integer> subList = new LinkedList<Integer>();
-            for(int i=0;i<levelNum;i++) {
-                if(queue.peek().left != null) queue.offer(queue.peek().left);
-                if(queue.peek().right != null) queue.offer(queue.peek().right);
-                subList.add(queue.poll().val);
-            }
-            lists.add(subList);
-        }
-        return lists;
+public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        if(left.val != right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }

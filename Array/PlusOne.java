8f1b462f57da668905a4a43e0789f6da3f11af87
Sang Lin <linsang90@gmail.com>
2015-09-15 19:06:38 -0400

    ends when no carry

--- Array/PlusOne.java
+++ Array/PlusOne.java
@@ -1,26 +1,14 @@
  public int[] plusOne(int[] digits) {
-        int mark = 0;
-        for(int i=digits.length-1;i>=0;i--){
-            int initSum = 0;
-            if(i==digits.length-1) initSum = digits[i]+1;
-            else {
-            initSum = digits[i]+mark;
+        public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-            mark = 0;
-            if(initSum>9){
-                initSum -=10;
-                mark = 1;
-                digits[i] = initSum;
-                if(i==0){
-                    int[] res = new int[digits.length+1];
-                    res[0] = 1;
-                    for(int j=0;j<digits.length-1;j++) {
-                        res[j+1] = digits[j];
-                    }
-                    return res;
-                }
-            }
-            digits[i] = initSum;
+            digits[i] = 0;
         }
-        return digits;
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
     }
+}

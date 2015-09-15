89850d34e60a7e13baf87f0793b138b57aa53833
Sang Lin <linsang90@gmail.com>
2015-09-15 19:07:32 -0400

    Update PlusOne.java

--- Array/PlusOne.java
+++ Array/PlusOne.java
@@ -1,14 +1,12 @@
- public int[] plusOne(int[] digits) {
-        public int[] plusOne(int[] digits) {
-        for(int i = digits.length - 1; i >= 0; i--) {
-            if(digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
+public int[] plusOne(int[] digits) {
+    for(int i = digits.length - 1; i >= 0; i--) {
+        if(digits[i] < 9) {
+             digits[i]++;
+             return digits;
         }
-        int[] res = new int[digits.length + 1];
-        res[0] = 1;
-        return res;
+        digits[i] = 0;
     }
+    int[] res = new int[digits.length + 1];
+    res[0] = 1;
+    return res;
 }

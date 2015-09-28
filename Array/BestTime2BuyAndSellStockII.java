e1606ca66f25a954c5a3dfa667f44ef762896823
Sang Lin <linsang90@gmail.com>
2015-09-28 16:00:24 -0400

    add all profit

---
+++ Array/BestTime2BuyAndSellStockII.java
@@ -0,0 +1,7 @@
+public int maxProfit(int[] prices) {
+        int maxPro = 0;
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] > prices[i - 1]) maxPro += (prices[i] - prices[i - 1]);
+        }
+        return maxPro;
+    }

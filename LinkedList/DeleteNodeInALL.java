18cebfd9b45d0252c03f8e7d855988c8fa355e8c
Sang Lin <linsang90@gmail.com>
2015-10-23 14:56:13 -0400

    move val backward

---
+++ LinkedList/DeleteNodeInALL.java
@@ -0,0 +1,9 @@
+public void deleteNode(ListNode node) {
+        ListNode prev = null;
+        while(node.next != null) {
+            node.val = node.next.val;
+            prev = node;
+            node = node.next;
+        }
+        prev.next = null;
+    }

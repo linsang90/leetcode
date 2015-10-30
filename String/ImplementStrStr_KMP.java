a497202b620251ddd42f1ceb15bcbcfb66122d5b
Sang Lin <linsang90@gmail.com>
2015-10-30 00:03:54 -0400

    kmp

---
+++ String/ImplementStrStr_KMP.java
@@ -0,0 +1,42 @@
+public int strStr(String haystack, String needle) {
+        if(haystack == null || needle == null) return 0;
+        
+        int h = haystack.length();
+        int n = needle.length();
+        
+        if (n > h) return -1;
+	    if (n == 0) return 0;
+
+        int[] next = getNext(needle);
+        int i = 0;
+        
+        while(i <= h - n) {
+            boolean matched = true;
+            for(int j = 0; j < n; j++) {
+                if(needle.charAt(0) != haystack.charAt(i)) {
+                    matched = false;
+                    i++;
+                    break;
+                } else if(needle.charAt(j) != haystack.charAt(i + j)) {
+                    matched = false;
+                    i = i + j  - next[j - 1];
+                    break;
+                }
+            }
+            if(matched) return i;
+        }
+        return -1;
+    }
+    
+    private int[] getNext(String needle) {
+        int[] next = new int[needle.length()];
+        next[0] = 0;
+        for(int i = 1; i < needle.length(); i++) {
+            int index = next[i - 1];
+            while(index > 0 && needle.charAt(index) != needle.charAt(i))
+                index = next[index - 1];
+            if(needle.charAt(index) == needle.charAt(i)) next[i] = next[i - 1] + 1;
+            else next[i] = 0;
+        }
+        return next;
+    }

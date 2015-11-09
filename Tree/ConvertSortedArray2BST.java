95846d13373e962c239b1351f82e191a6e4c91bc
Sang Lin <linsang90@gmail.com>
2015-11-09 13:41:54 -0800

    binary search

---
+++ Tree/ConvertSortedArray2BST.java
@@ -0,0 +1,13 @@
+public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null) return null;
+        return helper(nums, 0 , nums.length - 1);
+    }
+    
+    private TreeNode helper(int[] nums, int start, int end) {
+        if(start > end) return null;
+        int mid = (start + end) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(nums, start, mid - 1);
+        node.right = helper(nums, mid + 1, end);
+        return node;
+    }

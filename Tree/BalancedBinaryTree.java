c50c4c5a95d3b7d1425d12d9f25c67c0c8adfaf1
Sang Lin <linsang90@gmail.com>
2015-11-09 22:35:02 -0800

    .

---
+++ Tree/BalancedBinaryTree.java
@@ -0,0 +1,13 @@
+public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+    
+    private int getHeight(TreeNode root) {
+        if(root == null) return 0;
+        int left = getHeight(root.left);
+        if(left == -1) return -1;
+        int right = getHeight(root.right);
+        if(right == -1) return -1;
+        
+        return Math.abs(left - right) > 1? -1 : Math.max(left, right) + 1;
+    }

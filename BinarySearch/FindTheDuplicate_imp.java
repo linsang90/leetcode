ff69de4c8ed96b7f0b640ddbee6398d730d549a6
Sang Lin <linsang90@gmail.com>
2015-11-24 14:56:30 -0800

    first

---
+++ BinarySearch/FindTheDuplicate_imp.java
@@ -0,0 +1,21 @@
+public int findDuplicate(int[] nums) {
+        int n = nums.length - 1;
+        if(n > 0) {
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            
+            while(slow != fast) {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+            
+            slow = 0;
+            while(slow != fast) {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+            
+            return fast;
+        }
+      return -1;
+    }

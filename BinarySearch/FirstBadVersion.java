5d22a6c177c00e58222fd3d5186ac483ecd12631
Sang Lin <linsang90@gmail.com>
2015-11-24 10:55:11 -0800

    first

---
+++ BinarySearch/FirstBadVersion.java
@@ -0,0 +1,9 @@
+public int firstBadVersion(int n) {
+        int start = 1, end = n;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(!isBadVersion(mid)) start = mid + 1;
+            else end = mid;
+        }
+        return start;
+    }

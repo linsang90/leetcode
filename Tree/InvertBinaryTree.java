72f7ad1f3b616b7357a539ee354a336c5eb0841b
Sang Lin <linsang90@gmail.com>
2015-12-02 11:32:12 -0800

    thrid

---
+++ Tree/InvertBinaryTree.java
@@ -0,0 +1,8 @@
+ public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode left = invertTree(root.right);
+        TreeNode right = invertTree(root.left);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
